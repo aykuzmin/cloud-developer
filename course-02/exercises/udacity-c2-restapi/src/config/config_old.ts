faa7982c7306cdf437c5ef3ed925e0d3dca66f93
aykuzmin <devkuzmin@gmail.com>
2021-11-28 14:23:27 +0100

    part 2 lessons up to 4

---
+++ course-02/exercises/udacity-c2-restapi/src/config/config_old.ts
@@ -0,0 +1,20 @@
+export const config = {
+    "dev": {
+      "username": "udagramkuzmindev",
+      "password": "udagramkuzmindev",
+      "database": "udagramkuzmindev",
+      "host": "udagramkuzmindev.ctkbqyjjxbsr.us-east-1.rds.amazonaws.com",
+      "dialect": "postgres",
+      "aws_region": "us-east-1",
+      "aws_profile": "default",
+      "aws_media_bucket": "udagram-kuzmin-dev"
+    },
+    "prod": {
+      "username": "",
+      "password": "",
+      "database": "udagram_prod",
+      "host": "",
+      "dialect": "postgres"
+    }
+  }
+  
